6fc17d1518985c694aedd8381c2e771475b990b4
Elroy Hui <huielroy@hotmail.com>
2022-03-16 15:01:11 -0400

    added +snake that transmits name to server as soon as we connect

--- client.js
+++ client.js
@@ -9,6 +9,11 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: bbh");
+  });
+
   conn.on("data", (data) => {
     console.log(data);
   });
